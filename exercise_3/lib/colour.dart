ff11745dcdcfec40cbdc672921c62fb075dea553
Anis Najihah BeSquare <anisnjhahalim@gmail.com>
2021-10-28 15:28:02 +0800

    Day 3 exercise

---
+++ exercise_3/lib/colour.dart
@@ -0,0 +1,8 @@
+/*import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle kBodyText = TextStyle(fontSize: 22, color: Color.white);
+const Color kWhite = Colors.white;
+const Color kBlue = Color(0xff5663ff);*/
--- exercise_3/lib/main.dart
+++ exercise_3/lib/main.dart
@@ -9,20 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sign Up Page',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        //textTheme: GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme) ,
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Welcome Page'),
     );
   }
 }
@@ -46,17 +38,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? username;
+  String? email;
+  String? password;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final textController1 = TextEditingController();
+  final textController2 = TextEditingController();
+  final textController3 = TextEditingController();
+
+  var valueGender;
+
+  List listGender = ["Male", "Female"];
+
+  final String _imageUrl =
+      'https://i.ytimg.com/vi/ImC2Rf2sJRA/maxresdefault.jpg';
+
+  ///Class to demonstrate how to use Image, Snackbar, and Spier
+  /*class ImageSnackBarSpinner extends StatelessWidget {
+    final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = 
+    GlobalKey<ScaffoldMessengerState>();
+    final String _imageUrl = 'https://i.ytimg.com/vi/ImC2Rf2sJRA/maxresdefault.jpg';*/
+  textChecking() {
+    String text1, text2, text3;
+
+    text1 = textController1.text;
+    text2 = textController2.text;
+    text3 = textController3.text;
+
+    /*if (text1 == textController1.text.isNotEmpty ||
+        text2 == textController2.text.isNotEmpty ||
+        text3 == textController3.text.isNotEmpty) {
+      final SnackBar1 = SnackBar(
+        content: const Text('Register Successful'),
+        action: SnackBarAction(
+          label: 'close',
+          onPressed: () {},
+        ),
+      );
+    }*/
+
+    if (text1 == '' || text2 == '' || text3 == '') {
+      final snackBar = SnackBar(
+        content: const Text('Please fill up all the field'),
+        action: SnackBarAction(
+          label: 'close',
+          onPressed: () {},
+        ),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      final snackBar = SnackBar(
+        content: const Text("You have successfully registered!"),
+         action: SnackBarAction(
+          label: 'close',
+          onPressed: () {},
+        ),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   @override
@@ -71,43 +111,137 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+          child: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Image.network(
+              _imageUrl,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            /*Image.asset(
+            'images/deriv-logo.png'
+            ),*/
+            /* SizedBox(
+            height: 80.0,
+          ),*/
+            /* CircleAvatar(
+            backgroundColor: Colors.blue,
+            radius: 50.0,
+          ),*/
+            ///box for username
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: TextField(
+                controller: textController1,
+                 autofocus: false,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  labelText: 'Username',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    username = value;
+                  });
+                },
+              ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: TextField(
+                controller: textController2,
+                autofocus: false,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  labelText: 'Email',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: TextField(
+                controller: textController3,
+                 autofocus: false,
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  labelText: 'Password',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+            ),
+             
+            DropdownButton(
+              value: valueGender,
+              onChanged: (newValue) {
+                setState(() {
+                  valueGender = newValue;
+                });
+              },
+              items: listGender.map((valueGender) {
+                return DropdownMenuItem(
+                  value: valueGender,
+                  child: Text(valueGender),
+                );
+              }).toList(),
+            ),
+
+            ///register button to be pressed
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: MaterialButton(
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.blue,
+                onPressed: textChecking,
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          // child: Image.network(_imageUrl)(
+
+          ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
